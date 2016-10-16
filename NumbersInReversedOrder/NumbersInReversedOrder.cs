289f7d97dfe493064a71fb39eb0a67024c8843c5
didoff <dlucky95@gmail.com>
2016-10-17 01:45:14 +0300

    NumbersInReversedOrder finished

---
+++ NumbersInReversedOrder/NumbersInReversedOrder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace NumbersInReversedOrder
+{
+    class NumbersInReversedOrder
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Console.WriteLine(ReverseIntAndDouble(input));
+        }
+
+        private static string ReverseIntAndDouble(string s)
+        {
+            char[] charArray = s.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+    }
+}
