596f8542961c9a46b61944498606bdc711c73866
didoff <dlucky95@gmail.com>
2016-10-17 00:12:32 +0300

    LastDigit method complete

---
+++ MaxMethod/MaxMethod.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MaxMethod
+{
+    class MaxMethod
+    {
+        static void Main(string[] args)
+        {
+            int numberA = int.Parse(Console.ReadLine());
+            int numberB = int.Parse(Console.ReadLine());
+            int numberC = int.Parse(Console.ReadLine());
+            
+            int result = GetMax(numberA, numberB);
+            Console.WriteLine(GetMax(result, numberC));
+        }
+
+        private static int GetMax(int a, int b)
+        {
+            if (a > b)
+            {
+                return a;
+            }
+            else
+                return b;
+        }
+
+            
+    }
+}
