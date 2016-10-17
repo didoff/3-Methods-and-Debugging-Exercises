5185ca4461172420d0fc868db011754fea1ae05f
didoff <dlucky95@gmail.com>
2016-10-17 19:53:18 +0300

    Fib(n) and PrimeChecker (not 100/100)

---
+++ FibonacciNumbers/FibonacciNumbers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace FibonacciNumbers
+{
+    class FibonacciNumbers
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(GetNextFibNumber(n));
+        }
+
+        private static int GetNextFibNumber(int n)
+        {
+            int a = 1; // it not the real begining of the fibionacci number; should be 0;
+            int b = 1;
+            for (int i = 0; i < n; i++)
+            {
+                int temp = a;
+                a = b;
+                b = temp + b;
+            }
+
+            return a;
+        }
+    }
+}
---
+++ PriceChecker/PrimeChecker.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace PriceChecker
+{
+    class PrimeChecker // 87/100 in JUDGE!!! :O
+    {
+        static void Main(string[] args)
+        {
+            ulong n = ulong.Parse(Console.ReadLine());
+
+            Console.WriteLine(IsPrime(n));
+        }
+
+        private static bool IsPrime(ulong n)
+        {
+
+            if (n <= 1)
+            {
+                return false;
+            }
+
+            for (int i = 2; i <= Math.Sqrt(n); i++)
+            {
+                //if (n % i == 0 && i != n)
+                //{
+                //    return false;
+                //}
+                if ((int)n % i == 0)
+                {
+                    return false;
+
+                }
+            
+            }
+            return true;
+        }
+    }
+}
